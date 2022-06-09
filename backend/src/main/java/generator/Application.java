64fcc45d96bb7b8832fc9fca76589ed532b02918
王明鑫 <1658886033@qq.com>
2022-06-09 16:21:15 +0800

    增加全局异常捕捉、自定义异常

--- backend/src/main/java/generator/Application.java
+++ backend/src/main/java/generator/Application.java
@@ -12,5 +12,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
+    System.out.println("三巨头，永无BUG！");
   }
 }
---
+++ backend/src/main/java/generator/domain/JsonResult.java
@@ -0,0 +1,39 @@
+package generator.domain;
+
+public class JsonResult {
+    /**
+     * 异常码
+     */
+    protected String code;
+
+    /**
+     * 异常信息
+     */
+    protected String msg;
+
+    public JsonResult() {
+        this.code = "200";
+        this.msg = "操作成功";
+    }
+
+    public JsonResult(String code, String msg) {
+        this.code = code;
+        this.msg = msg;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
