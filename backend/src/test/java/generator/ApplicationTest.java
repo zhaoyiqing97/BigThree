a048ff6664ccafd26c051e47904f29de7efcc17a
刘斌 <liu_bin4835@outlook.com>
2022-08-04 09:08:02 +0800

    update test

--- backend/src/test/java/generator/ApplicationTest.java
+++ backend/src/test/java/generator/ApplicationTest.java
@@ -1,9 +1,13 @@
 package generator;
 
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Lists;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import lombok.val;
 
@@ -13,7 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class ApplicationTest {
 
   @Test
-  void contextLoads() {
+  void finsElement() {
     val str = "3";
     val list = Lists.newArrayList("1", "2", "3");
     val b = list.stream().anyMatch(s -> s.equals(str));
@@ -25,5 +29,27 @@ class ApplicationTest {
     val dateStr = "202207";
     val insert = new StringBuilder(dateStr).insert(4, "-");
     assertEquals(insert.toString(), "2022-07");
+  }
+
+  @Test
+  void mergeListStream(){
+    val a = Lists.newArrayList("1", "2", "3");
+    val b = Lists.newArrayList("4", "42", "43");
+    val c = Stream.concat(a.stream(),b.stream()).collect(Collectors.toList());
+    System.out.println("a = " + a);
+    System.out.println("b = " + b);
+    System.out.println("c = " + c);
+    assertEquals(Lists.newArrayList("1", "2", "3","4", "42", "43"), c);
+  }
+
+  @Test
+  void mergeListGuava(){
+    val a = Lists.newArrayList("1", "2", "3");
+    val b = Lists.newArrayList("4", "42", "43");
+    val c = Lists.newArrayList(Iterables.concat(a, b));
+    System.out.println("a = " + a);
+    System.out.println("b = " + b);
+    System.out.println("c = " + c);
+    assertEquals(Lists.newArrayList("1", "2", "3","4", "42", "43"), c);
   }
 }
