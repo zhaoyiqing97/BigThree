217a77cf03f6c822051e5d17856c9a4da9698e7a
刘斌 <liu_bin4835@outlook.com>
2022-08-22 15:55:33 +0800

    update version to 2.0.0

--- ops/build/build-images.sh
+++ ops/build/build-images.sh
@@ -8,7 +8,7 @@ for i in $list; do
     -H "Accept: application/vnd.github+json" \
     -H "Authorization: token $GITHUB_TOKEN" \
     https://api.github.com/repos/zhaoyiqing97/BigThree/actions/workflows/"$i"/dispatches \
-    -d '{"ref":"main","inputs":{"version":"1.0.0"}}'
+    -d '{"ref":"main","inputs":{"version":"2.0.0"}}'
   echo "$i" is success
 done
 
