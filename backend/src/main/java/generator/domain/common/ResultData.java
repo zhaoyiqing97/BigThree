ecbfa17353610aaf514f1cc42dc9c4a5afb8045b
liubin <liu_bin4835@outlook.com>
2022-11-09 16:08:42 +0800

    add skywalking log

--- backend/src/main/java/generator/domain/common/ResultData.java
+++ backend/src/main/java/generator/domain/common/ResultData.java
@@ -1,5 +1,7 @@
 package generator.domain.common;
 
+import org.apache.skywalking.apm.toolkit.trace.TraceContext;
+
 import lombok.Data;
 
 /**
@@ -8,45 +10,50 @@ import lombok.Data;
 @Data
 public class ResultData<T> {
 
-    private Integer code;
-    private String msg;
-    private T data;
+  private Integer code;
+  private String msg;
+  private T data;
+  private String traceId;
 
-    /**
-     * 返回成功
-     */
-    public static <T> ResultData<T> success() {
-        ResultData<T> resultData = new ResultData<>();
-        resultData.setCode(ResultCode.SUCCESS.getCode());
-        resultData.setMsg(ResultCode.SUCCESS.getMessage());
-        return resultData;
-    }
+  /**
+   * 返回成功
+   */
+  public static <T> ResultData<T> success() {
+    ResultData<T> resultData = new ResultData<>();
+    resultData.setCode(ResultCode.SUCCESS.getCode());
+    resultData.setMsg(ResultCode.SUCCESS.getMessage());
+    resultData.setTraceId(TraceContext.traceId());
+    return resultData;
+  }
 
-    /**
-     * 返回成功
-     */
-    public static <T> ResultData<T> success(T result) {
-        ResultData<T> resultData = ResultData.success();
-        resultData.setData(result);
-        return resultData;
-    }
+  /**
+   * 返回成功
+   */
+  public static <T> ResultData<T> success(T result) {
+    ResultData<T> resultData = ResultData.success();
+    resultData.setData(result);
+    resultData.setTraceId(TraceContext.traceId());
+    return resultData;
+  }
 
-    /**
-     * 返回失败
-     */
-    public static <T> ResultData<T> error() {
-        ResultData<T> resultData = new ResultData<>();
-        resultData.setCode(ResultCode.UNKNOWN_ERROR.getCode());
-        resultData.setMsg(ResultCode.UNKNOWN_ERROR.getMessage());
-        return resultData;
-    }
+  /**
+   * 返回失败
+   */
+  public static <T> ResultData<T> error() {
+    ResultData<T> resultData = new ResultData<>();
+    resultData.setCode(ResultCode.UNKNOWN_ERROR.getCode());
+    resultData.setMsg(ResultCode.UNKNOWN_ERROR.getMessage());
+    resultData.setTraceId(TraceContext.traceId());
+    return resultData;
+  }
 
-    /**
-     * 返回失败
-     */
-    public static <T> ResultData<T> error(T result) {
-        ResultData<T> resultData = ResultData.error();
-        resultData.setData(result);
-        return resultData;
-    }
+  /**
+   * 返回失败
+   */
+  public static <T> ResultData<T> error(T result) {
+    ResultData<T> resultData = ResultData.error();
+    resultData.setData(result);
+    resultData.setTraceId(TraceContext.traceId());
+    return resultData;
+  }
 }
